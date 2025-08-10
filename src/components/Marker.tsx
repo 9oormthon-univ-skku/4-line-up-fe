b273f54439270e04123ef9fe5c4500dccb34b90a
GunderGie <rjs94951@gmail.com>
2025-08-10 19:59:43 +0900

    feat: Make svg icons rendered with url

--- src/components/Marker.tsx
+++ src/components/Marker.tsx
@@ -2,6 +2,7 @@ import { colors, shadows } from '@/styles/styles';
 import { css } from '@emotion/react';
 import DefaultMarkerIcon from '@icons/marker-icon-default.svg?react';
 import MarkerPin from '@icons/marker.svg?react';
+import SvgInline from './SvgInline';
 
 const markerCss = css`
   color: ${colors.primary};
@@ -35,9 +36,13 @@ const Marker = ({ selected = false, iconUrl }: MarkerProps) => {
   return (
     <div css={[markerCss, selected && selectedCss]}>
       <MarkerPin />
-      <div>
-        <DefaultMarkerIcon />
-      </div>
+      {iconUrl ? (
+        <SvgInline url={iconUrl} />
+      ) : (
+        <div>
+          <DefaultMarkerIcon />
+        </div>
+      )}
     </div>
   );
 };
