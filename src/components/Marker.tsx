fabc4d6bc85eb84f0be5a00d4a29950d3e0c81fe
GunderGie <rjs94951@gmail.com>
2025-08-11 21:15:43 +0900

    feat: Apply pinch-zoom and its controls

--- src/components/Marker.tsx
+++ src/components/Marker.tsx
@@ -3,8 +3,11 @@ import { css } from '@emotion/react';
 import DefaultMarkerIcon from '@icons/marker-icon-default.svg?react';
 import MarkerPin from '@icons/marker.svg?react';
 import SvgInline from './SvgInline';
+import type { ComponentProps } from 'react';
 
 const markerCss = css`
+  width: fit-content;
+  height: fit-content;
   color: ${colors.primary};
   ${shadows.dropBottom}
   path {
@@ -30,14 +33,14 @@ const selectedCss = css`
   }
 `;
 
-interface MarkerProps {
+interface MarkerProps extends ComponentProps<'div'> {
   selected?: boolean;
   iconUrl?: string;
 }
 
-const Marker = ({ selected = false, iconUrl }: MarkerProps) => {
+const Marker = ({ selected = false, iconUrl, ...props }: MarkerProps) => {
   return (
-    <div css={[markerCss, selected && selectedCss]}>
+    <div id={props.id} onClick={props.onClick} css={[markerCss, selected && selectedCss]}>
       <MarkerPin />
       {iconUrl ? (
         <SvgInline url={iconUrl} />
--- src/pages/map/MapPage.tsx
+++ src/pages/map/MapPage.tsx
@@ -1,18 +1,47 @@
 import Marker from '@/components/Marker';
 import { css } from '@emotion/react';
+import { colors } from '@/styles/styles';
+import { useRef } from 'react';
 import MapDrawer from './mapDrawer/MapDrawer';
+import {
+  TransformComponent,
+  TransformWrapper,
+  type ReactZoomPanPinchRef,
+} from 'react-zoom-pan-pinch';
+
+const wrapperSt = {
+  height: '100vh',
+  width: '100%',
+  backgroundColor: colors.primary10,
+};
+const contentSt = {
+  height: '100%',
+  width: '100%',
+};
 
 const MapPage = () => {
+  const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null);
+  const zoomTo = (elementId: string, scale?: number) => {
+    if (!transformComponentRef.current) {
+      return;
+    }
+    const { zoomToElement } = transformComponentRef.current;
+    console.log('zoomTo', elementId);
+    zoomToElement(elementId, scale ?? 3.0);
+  };
+
   return (
-    <div css={css``}>
-      <Marker />
-      <Marker selected />
+    <TransformWrapper ref={transformComponentRef}>
+      <TransformComponent wrapperStyle={wrapperSt} contentStyle={contentSt}>
+        <Marker id='m1' onClick={() => zoomTo('m1', 1.5)} />
+        <Marker id='m2' onClick={() => zoomTo('m2', 1.5)} selected />
+      </TransformComponent>
       <MapDrawer>
-        <div>Booth List Element 1</div>
-        <div>Booth List Element 2</div>
+        <div onClick={() => zoomTo('m1')}>Booth List Element 1</div>
+        <div onClick={() => zoomTo('m2')}>Booth List Element 2</div>
         <div>Booth List Element 3</div>
       </MapDrawer>
-    </div>
+    </TransformWrapper>
   );
 };
 
