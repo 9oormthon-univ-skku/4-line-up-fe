10f2632261330ae3c0f55edfde626fdb4d38548b
GunderGie <rjs94951@gmail.com>
2025-08-19 15:06:50 +0900

    feat: Add Axios

---
+++ src/api/axios.ts
@@ -0,0 +1,31 @@
+import axios, { type AxiosResponse } from 'axios';
+
+const apiUrl = import.meta.env.VITE_API;
+
+const axiosInstance = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+class BadResponseFormatError extends Error {
+  constructor(response: AxiosResponse) {
+    super(`Malformed response: ${response.headers['content-type']}`);
+  }
+}
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    if (response.headers['content-type'] !== 'application/json') {
+      throw new BadResponseFormatError(response);
+    }
+    return response;
+  },
+  (error) => {
+    console.error('API call failed:', error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
---
+++ src/api/endpoints.ts
@@ -0,0 +1,8 @@
+export const API_ENDPOINTS = {
+  GET_AREAS: '/areas',
+  GET_BOOTHS: '/markers',
+  GET_CATEGORIES: '/categories',
+  GET_MENUS: '/stores/:store_id/menus',
+  GET_POSTS: '/posts',
+  GET_TIMETABLE: '/timetable',
+};
