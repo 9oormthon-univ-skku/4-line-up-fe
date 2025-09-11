91c0f1a5bfb02a3cd5c547b70cefb301a8346405
Dongkun Kim <80196807+GunderGie@users.noreply.github.com>
2025-09-12 03:49:42 +0900

    feat: Sort BoothList, etc. (#24)
    * feat: handle link doesn't exist in BoothDetail
    
    * feat: Add discalimer on SNB
    
    * feat: Sort BoothList based on map transform position

--- src/components/SideNavBar/SideNavBarPanel.tsx
+++ src/components/SideNavBar/SideNavBarPanel.tsx
@@ -1,52 +1,61 @@
-import { colors, shadows } from '@/styles/styles';
+import { disclaimerText } from '@/constants';
+import { colors, fonts, shadows } from '@/styles/styles';
 import { css } from '@emotion/react';
 import Bg from '@images/bg-stars.svg?react';
-import Logo from '@images/logo-9u.svg?react'
+import Logo from '@images/logo-9u.svg?react';
+
+const SideNavBarPanelCss = css`
+  height: 100lvh;
+  width: 100%;
+  padding: 5.4rem 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  z-index: -20;
+  background-color: ${colors.white};
+  border-left: 2px solid ${colors.primary};
+  box-shadow: ${shadows.left};
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+  .snb-footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+    margin-bottom: 6rem;
+  }
+  .disclaimer {
+    ${fonts.label_xsm};
+    color: ${colors.primary30};
+  }
+`;
 
 const SideNavBarPanel = (props: React.HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div
-      css={css`
-        height: 100lvh;
-        width: 100%;
-        padding: 5.4rem 16px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        position: fixed;
-        z-index: -20;
-        background-color: ${colors.white};
-        border-left: 2px solid ${colors.primary};
-        box-shadow: ${shadows.left};
-      `}
-    >
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          gap: 20px;
-        `}
-      >
-        {props.children}
-      </div>
-      <p
-        css={css`
-          margin-bottom: 10rem;
-        `}
-      >
+    <div css={SideNavBarPanelCss}>
+      <div>{props.children}</div>
+      <p className='snb-footer'>
         <Bg
           css={css`
             color: ${colors.primary20};
-            position: fixed;
+            position: absolute;
             bottom: 0;
             right: 0;
             z-index: -10;
           `}
         />
-        <Logo css={css`
-          color: ${colors.primary30};
-        `}/>
+        <Logo
+          css={css`
+            color: ${colors.primary30};
+          `}
+        />
+        <p className='disclaimer'>{disclaimerText}</p>
       </p>
     </div>
   );
--- src/constants.ts
+++ src/constants.ts
@@ -10,3 +10,5 @@ export const days = [
 
 export const BannerLinkUrl =
   'https://pf.kakao.com/_VPICn/friend?fbclid=PAZXh0bgNhZW0CMTEAAaeuhFM2BKXJz68LzaOAtR9UBTl902e9eT93txC5W83vIu2RJeh6eEsJja8dcw_aem_mWsbiFqllOCegHYX9moHYw';
+
+export const disclaimerText = '이 페이지의 컨텐츠는 성균관대학교 총학생회 S:CATCH의 제작물을 바탕으로 허가 하에 만들어졌습니다.'
