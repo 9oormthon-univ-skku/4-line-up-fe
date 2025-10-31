67dacf6dff24f9fdda4836efa332e28955fd2f97
GunderGie <rjs94951@gmail.com>
2025-10-31 17:03:56 +0900

    style: Change design of SideNavOpenBtn & BtnBack

--- src/components/SideNavBar/SideNavOpenBtn.tsx
+++ src/components/SideNavBar/SideNavOpenBtn.tsx
@@ -5,15 +5,6 @@ import HamburgerIcon from '@icons/hamburger.svg?react';
 const SideNavOpenBtn = (props:React.HTMLAttributes<HTMLDivElement>) => {
   return (
     <div css={css`
-        height: 6rem;
-        width: 12rem;
-        border-radius: 3rem;
-        border: 2px solid ${colors.primary};
-        background-color: ${colors.white};
-        ${shadows.dropBottom};
-        display: flex;
-        align-items: center;
-        z-index: -30;
         svg {
           height: 5.6rem;
           color: ${colors.primary};
--- src/components/icons/BtnBack.tsx
+++ src/components/icons/BtnBack.tsx
@@ -1,14 +1,17 @@
 import type { ComponentProps } from "react";
 import Arrow from '@icons/arrow-left.svg?react';
 import { css } from "@emotion/react";
-import { colors } from "@/styles/styles";
+import { colors, shadows } from "@/styles/styles";
 
 const BtnBack = ({...props}: ComponentProps<'button'>) => {
     return <button css={css`
         color: ${colors.primary};
+        background-color: ${colors.white};
+        ${shadows.dropCenter};
+        border-radius: 3rem;
         line-height: 0;
-        width: 4.8rem;
-        height: 4.8rem;
+        width: 5.6rem;
+        height: 5.6rem;
     `} aria-label='뒤로가기' {...props}>
         <Arrow/>
     </button>
